0c6ac17b93a05df688aec63b3243c369493618a2
Vincent Cantin <vincent@404.taipei>
2019-04-01 01:05:33 +0800

    Broke tests, fixed them.

--- test/hopen/runner.cljs
+++ test/hopen/runner.cljs
@@ -2,9 +2,9 @@
   (:require  [cljs.test :as t :include-macros true]
              [doo.runner :refer-macros [doo-all-tests doo-tests]]
              [hopen.renderer.xf-test]
-             [hopen.parser.util-test]
+             [hopen.syntax.util-test]
              [hopen.util-test]))
 
 (doo-tests 'hopen.renderer.xf-test
-           'hopen.parser.util-test
+           'hopen.syntax.util-test
            'hopen.util-test)
